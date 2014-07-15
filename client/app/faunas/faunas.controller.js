e00c229aa26beca91249695244822a66344fa957
mordrax <joseph.ni@gmail.com>
2014-07-15 19:12:48 +1000

    made flora/fauna views ngTable

--- client/app/faunas/faunas.controller.js
+++ client/app/faunas/faunas.controller.js
@@ -1,11 +1,29 @@
 'use strict';
 
 angular.module('simDdApp')
-    .controller('FaunasCtrl', function ($scope, WorldService) {
+    .controller('FaunasCtrl', function ($scope, WorldService, ngTableParams, $filter) {
         $scope.message = 'Hello';
 
-        WorldService.faunaService.query(function(result) {
-                $scope.faunas = result;
+        WorldService.faunaService.query(function (data) {
+            $scope.faunasTable = new ngTableParams({
+                page: 1,            // show first page
+                count: 10,          // count per page
+                filter: {
+                }
+            }, {
+                total: data.length, // length of data
+                getData: function ($defer, params) {
+                    // use build-in angular filter
+                    var orderedData = params.filter() ?
+                        $filter('filter')(data, params.filter()) :
+                        data;
+
+                    $scope.faunas = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+
+                    params.total(orderedData.length); // set total for recalc pagination
+                    $defer.resolve($scope.faunas);
+                }
+            });
         });
 
         $scope.save = function (data) {
--- client/app/floras/floras.controller.js
+++ client/app/floras/floras.controller.js
@@ -1,10 +1,28 @@
 'use strict';
 
 angular.module('simDdApp')
-    .controller('FlorasCtrl', function ($scope, WorldService) {
+    .controller('FlorasCtrl', function ($scope, WorldService, ngTableParams, $filter) {
         $scope.message = 'Hello';
 
-        WorldService.floraService.query(function (result) {
-            $scope.floras = result;
+        WorldService.floraService.query(function (data) {
+            $scope.florasTable = new ngTableParams({
+                page: 1,            // show first page
+                count: 10,          // count per page
+                filter: {
+                }
+            }, {
+                total: data.length, // length of data
+                getData: function ($defer, params) {
+                    // use build-in angular filter
+                    var orderedData = params.filter() ?
+                        $filter('filter')(data, params.filter()) :
+                        data;
+
+                    $scope.floras = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+
+                    params.total(orderedData.length); // set total for recalc pagination
+                    $defer.resolve($scope.floras);
+                }
+            });
         });
     });
