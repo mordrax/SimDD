ae0766a7134d90ec6ed9b7b48fa23c17c4adda8d
mordrax <joseph.ni@gmail.com>
2014-07-19 13:43:16 +1000

    added client world route

--- client/app/world/world.controller.js
+++ client/app/world/world.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('simDdApp')
     .controller('WorldCtrl', function ($scope, WorldService) {
-        WorldService.worldService.query({id:'7468654f6e65416e644f6c79'}, function (data) {
+        WorldService.worldService.query(function (data) {
             $scope.world = data;
         });
     });
